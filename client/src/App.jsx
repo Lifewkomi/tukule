d6fe0678af5b54353b2ee88526c622c38b74bcf7
Philemon Komi <philemonkomi46@gmail.com>
2025-04-05 23:18:00 +0300

    Merged main with admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { Light } from "./styles/themes";
 import Globals from "./styles/globals";
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 // Lazy-loaded page components
-// const Home = React.lazy(() => import("./pages/Home"));
-// const MainMenu = React.lazy(() => import("./pages/MainMenu"));
+const Home = React.lazy(() => import("./pages/Home"));
+const MainMenu = React.lazy(() => import("./pages/MainMenu"));
 const TableVisualization = React.lazy(() => import("./pages/Reservations"));
-// const Shop = React.lazy(() => import("./pages/Shop"));
-// const Contact = React.lazy(() => import("./pages/Contact"));
-// const AdminPanel = React.lazy(() => import("./pages/AdminPanel"));
+const Shop = React.lazy(() => import("./pages/Shop"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const AdminPanel = React.lazy(() => import("./pages/AdminPanel"));
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
       <ThemeProvider theme={Light}>
         <Globals />
         <main className="App">
-          {/* <React.Suspense fallback={<div>Loading...</div>}>
+          <React.Suspense fallback={<div>Loading...</div>}>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/menu" element={<MainMenu />} />
@@ -27,8 +27,7 @@ function App() {
               <Route path="/contacts" element={<Contact />} />
               <Route path="/admin" element={<AdminPanel />} />
             </Routes>
-          </React.Suspense> */}
-          <TableVisualization />
+          </React.Suspense>
         </main>
       </ThemeProvider>
     </>
